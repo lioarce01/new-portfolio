75582f376cffdaa8e136239c4f1623f6e88be9fd
lioarce01 <lioarce1@gmail.com>
2022-09-19 11:09:17 -0300

    new work added

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -9,15 +9,16 @@ import Card6 from "../components/Cards/Card6"
 import Card7 from "../components/Cards/Card7"
 import Card8 from "../components/Cards/Card8"
 import Card9 from "../components/Cards/Card9"
+import Card10 from "../components/Cards/Card10"
 
 const Works = () => {
   return (
     <div className="bg-[#141414] h-full text-start justify-center flex dark:bg-[#fcf9df]">
       <div className="w-[100vh]">
-        <div className="py-8 font-bold text-2xl">
+        <div className="py-8 text-2xl font-bold">
           <h1 className="text-gray-300 px-8 dark:text-[#141414]">Works</h1>
         </div>
-        <div className="grid-cols pl-6 md:grid md:grid-cols-2">
+        <div className="pl-6 grid-cols md:grid md:grid-cols-2">
           <Card1 />
           <Card2 />
           <Card3 />
@@ -27,6 +28,7 @@ const Works = () => {
           <Card7 />
           <Card8 />
           <Card9 />
+          <Card10 />
         </div>
         <div>
           <Contact />
